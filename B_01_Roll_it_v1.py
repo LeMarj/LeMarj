d3ee316a15ce22800f8a8418a374b6199048e93f
LeMarj <Hlawnceul3130@masseyhigh.school.nz>
2025-02-18 13:23:27 +1300

    vid 8

--- B_01_Roll_it_v1.py
+++ B_01_Roll_it_v1.py
@@ -26,7 +26,25 @@ Roll the dice and try to win!
     """)
 
 
+def int_check():
+    """Checks users enter an integer more than / equal to 13"""
+
+    error = "Please enter an integer more than / equal to 13"
+
+    while True:
+        try:
+            response = int(input("What is the game goal? "))
+
+            if response < 13:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
 # Main routine
+
 
 # ask the user if they want instructions (check they say yes / no)
 want_instructions = yes_no("Do you want to see the instructions? ")
@@ -35,5 +53,6 @@ want_instructions = yes_no("Do you want to see the instructions? ")
 if want_instructions == "yes":
     instructions()
 
-    print()
-    print("Program continues")
+print()
+game_goal = int_check()
+print(game_goal)

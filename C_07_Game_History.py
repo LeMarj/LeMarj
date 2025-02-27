51e06e2a7e5d43986a8f8679124cc194f42fa4e3
LeMarj <Hlawnceul3130@masseyhigh.school.nz>
2025-02-27 14:42:54 +1300

    checkpoint 3

--- C_07_Game_History.py
+++ C_07_Game_History.py
@@ -3,8 +3,6 @@
 game_history = []
 
 # get data (base component does this already, code below fore testing purposes)
-user_score = 0
-comp_score = 0
 
 while True:
     rounds_played = input("Round? ")
@@ -13,6 +11,17 @@ while True:
 
     user_points = int(input("User points? "))
     comp_points = int(input("Computer points? "))
+    winner = input("Who won? ")
+    user_score = int(input("User score: "))
+    comp_score = int(input("Computer score: "))
 
     game_results = (f"Round {rounds_played}: User Points {user_points} | "
-                    f"Computer Points {comp_points}, Useer wins (15 | 0)")
+                    f"Computer Points {comp_points}, {winner} wins "
+                    f"({user_score} | {comp_score})")
+
+    game_history.append(game_results)
+
+    print("Game History")
+
+    for item in game_history:
+        print(item)
